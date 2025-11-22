450b0abae794dc36dfadafc7e443263a7d023243
geekeydevil-boop <geekeydevil@gmail.com>
2025-11-22 15:54:16 -0500

    Update uservar.py

--- repo/plugin.program.openwizard/uservar.py
+++ repo/plugin.program.openwizard/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR limegreen][B]Open[/B][/COLOR]Wizard'
 BUILDERNAME = 'OpenWizard'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'https://github.com/geekeydevil-boop/kokobuilds.github.io/blob/master/builds.txt'
+BUILDFILE = 'https://raw.githubusercontent.com/geekeydevil-boop/kokobuilds.github.io/refs/heads/master/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
@@ -122,3 +122,4 @@ FONTSETTINGS = 'Font13'
 # Background for Notification Window
 BACKGROUND = 'http://'
 #########################################################
+
